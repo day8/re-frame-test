62e4c9c40a646d247360b4e8a5899a2caa5c624e
Daniel Compton <desk@danielcompton.net>
2016-07-25 14:57:12 +1200

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to re-frame-test
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/re_frame_test/core_test.clj
@@ -0,0 +1,7 @@
+(ns re-frame-test.core-test
+  (:require [clojure.test :refer :all]
+            [re-frame-test.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
