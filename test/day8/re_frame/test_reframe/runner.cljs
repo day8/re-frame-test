56ac55e4a48b8735b5ed947f88ef5e7ba7f78bb4
Sam Roberton <sam.roberton@rokt.com>
2016-08-18 19:19:47 +1000

    Fix karma runner for changed test namespace
    (The namespace changed to follow the `-test` convention so that tooling
    such as CIDER automatically understands it.)

--- test/day8/re_frame/test_reframe/runner.cljs
+++ test/day8/re_frame/test_reframe/runner.cljs
@@ -1,8 +1,8 @@
 (ns day8.re-frame.test-reframe.runner
   (:require [jx.reporter.karma :as karma :include-macros true]
-            [day8.re-frame.test-reframe.test]))
+            [day8.re-frame.test-reframe.test-test]))
 
 (defn ^:export run-karma [karma]
   (karma/run-tests
     karma
-    'day8.re-frame.test-reframe.test))
+    'day8.re-frame.test-reframe.test-test))
