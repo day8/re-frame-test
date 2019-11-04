767a6a00aaf4e6b9768a15b2dd609642391216c7
Isaac Johnston <isaac@superstruct.nz>
2019-11-04 11:33:14 +0800

    Upgrade shadow-cljs to 2.8.69

--- project.clj
+++ project.clj
@@ -20,7 +20,7 @@
                  [org.clojure/clojurescript "1.10.520" :scope "provided"
                   :exclusions [com.google.javascript/closure-compiler-unshaded
                                org.clojure/google-closure-library]]
-                 [thheller/shadow-cljs "2.8.67" :scope "provided"]
+                 [thheller/shadow-cljs "2.8.69" :scope "provided"]
                  [re-frame "0.10.9"]]
   
   :plugins [[me.arrdem/lein-git-version "2.0.3"]
